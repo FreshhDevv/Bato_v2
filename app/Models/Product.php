4fcd17a0087182adbed2614f4a5571197d37f0ed
FreshDev <billyhans90@gmail.com>
2022-06-07 17:33:45 +0100

    Add the relationships between the tables

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'body',
+        'user_id',
+        'image',
+    ];
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- database/migrations/2022_06_07_162010_create_products_table.php
+++ database/migrations/2022_06_07_162010_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('body');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
